7c3a2fafb2c4464fc6b1baca2077e321266f4f2f
Ajay Patel <ajay.patel@data-axle.com>
2024-04-27 22:06:15 +0530

    Assignment of class 6

---
+++ python/class6/assignment.py
@@ -0,0 +1,43 @@
+# Create 3 examples of classes without static variables
+# Create 3 more examples of classes with static variables
+
+class Logger:
+    DEBUG = 10
+    INFO = 20
+    WARNING = 30
+    ERROR = 40
+    CRITICAL = 50
+    TOTAL_INSTANCE_CREATED=0
+
+    def __init__(self, level, name):
+        self.log_level = level
+        self.log_name= name
+        Logger.TOTAL_INSTANCE_CREATED += 1
+
+# Using logger
+logger = Logger(Logger.ERROR, "looger1")
+Logger(Logger.DEBUG, "looger2")
+print("log level:",logger.log_level)
+print("logger name:",logger.log_name)
+print("total log instances : ", logger.TOTAL_INSTANCE_CREATED)
+
+
+class NetworkConfig:
+    SERVER_ADDRESS = "127.0.0.1"
+    DEFAULT_PORT = 65500
+    DEFAULT_TIMEOUT = 30
+
+    def __init__(self, port=None, timeout=None):
+        self.port = port if port is not None else NetworkConfig.DEFAULT_PORT
+        self.timeout = timeout if timeout is not None else NetworkConfig.DEFAULT_TIMEOUT
+
+nconf =NetworkConfig()
+print("Server address:", nconf.SERVER_ADDRESS)
+print("Port:", nconf.port)
+print("timeout:", nconf.timeout)
+
+
+nconf1 =NetworkConfig(22, 15)
+print("Server address:", nconf1.SERVER_ADDRESS)
+print("Port:", nconf1.port)
+print("timeout:", nconf1.timeout)
